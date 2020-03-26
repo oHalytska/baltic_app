ad0c7678d6d99619248a15cb3dd863c610cd5926
Olha Halytska <halytska.ol@gmail.com>
2020-03-26 21:42:55 +0200

    evening commit

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.set('views views');
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+  res.render('index', { country: {} });
+});
+
+/*8app.get('/', (req, res) => {res.render('index', { time: '' });
+});*/
 
 app.post('/', function(req, res) {
   const country = req.body.countries;
@@ -18,9 +23,10 @@ app.post('/', function(req, res) {
   axios
     .get(url)
     .then(function(response) {
+      console.log(response.data);
+      const country = response.data[0];
       console.log(country);
-      const capital = response.data.bpi[country];
-      console.log(capital);
+      res.render('index', { country });
     })
     .catch(function(error) {
       console.log(error);
