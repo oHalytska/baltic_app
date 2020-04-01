c2bae512843070039c62b4caf239e7ddae54d9a0
Olha Halytska <halytska.ol@gmail.com>
2020-04-01 22:49:55 +0300

    Final final

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views views');
 
 app.get('/', (req, res) => {
-  res.render('index', { country: {} });
+  res.render('index', {
+    country: {},
+    language: {},
+    code: {},
+    name: {},
+    symbol: {}
+  });
 });
 
 /*8app.get('/', (req, res) => {res.render('index', { time: '' });
@@ -24,9 +33,15 @@ app.post('/', function(req, res) {
     .get(url)
     .then(function(response) {
       const country = response.data[0];
-      console.log(country.languages[0].nativeName);
-      console.log(country.currencies[0].code);
-      res.render('index', { country });
+      //console.log(country.languages[0].nativeName);
+      //console.log(country.currencies[0].code);
+      res.render('index', {
+        country,
+        language: country.languages[0].nativeName,
+        code: country.currencies[0].code,
+        name: country.currencies[0].name,
+        symbol: country.currencies[0].symbol
+      });
     })
     .catch(function(error) {
       console.log(error);
